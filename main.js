0f6fd66928119e9d740854bad3df771005d93760
Janet Chu <janetpchu@berkeley.edu>
2015-10-31 13:54:58 -0700

    Added projects.

--- main.js
+++ main.js
@@ -4,19 +4,31 @@ mainApp.config(function($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'home.html',
-            controller: 'ctrl'
+            // controller: 'ctrl'
         })
         .when('/about', {
             templateUrl:'about.html',
-            controller: 'ctrl'
+            // controller: 'ctrl'
         })
         .when('/projects', {
             templateUrl: 'projects.html',
-            controller: 'ctrl'
+            // controller: 'ctrl'
         })
         .when('/contact', {
             templateUrl: 'contact.html',
-            controller: 'ctrl'
+            // controller: 'ctrl'
+        })
+        .when('/projects/cachinginspark', {
+            templateUrl: 'project-caching-spark.html'
+            // controller: 'ctrl' 
+        })
+        .when('/projects/digitrecognition', {
+            templateUrl: 'digit-recognition.html'
+            // controller: 'ctrl' 
+        })
+        .when('/projects/pinkthink', {
+            templateUrl: 'pinkthink.html'
+            // controller: 'ctrl' 
         })
         .otherwise({
             redirectTo: '/'
